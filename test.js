aabca9c26c372f2a6eab3909426e0948f63bcba1
Simen Bekkhus <sbekkhus91@gmail.com>
2017-06-02 17:17:44 +0200

    Fix tests on older nodes

--- test.js
+++ test.js
@@ -1,5 +1,4 @@
 /* eslint-env jest */
-/* eslint-disable node/no-unsupported-features */
 
 'use strict';
 
@@ -13,24 +12,24 @@ const func = shouldError => {
   return cb => cb(null, 'yay');
 };
 
-test('promisified function resolves', async () => {
+test('promisified function resolves', () => {
   const promisified = m(func());
-  await expect(promisified()).resolves.toBeDefined();
+  return expect(promisified()).resolves.toBeDefined();
 });
 
-test('promisified function rejects', async () => {
+test('promisified function rejects', () => {
   const promisified = m(func(true));
-  await expect(promisified()).rejects.toBeDefined();
+  return expect(promisified()).rejects.toBeDefined();
 });
 
-test('promisified object resolve', async () => {
+test('promisified object resolve', () => {
   const promisified = m({ someFunc: func() });
-  await expect(promisified.someFunc()).resolves.toBeDefined();
+  return expect(promisified.someFunc()).resolves.toBeDefined();
 });
 
-test('promisified object rejects', async () => {
+test('promisified object rejects', () => {
   const promisified = m({ someFunc: func(true) });
-  await expect(promisified.someFunc()).rejects.toBeDefined();
+  return expect(promisified.someFunc()).rejects.toBeDefined();
 });
 
 test('successfully promisify fs', () => {
